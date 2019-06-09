02bf82849f78d8b51831b08aad4296a4b40bfafd
Sakip Gur <msakipgur@gmail.com>
2019-06-09 18:56:46 -0400

    App change

--- client/src/App.js
+++ client/src/App.js
@@ -10,13 +10,13 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Fib Calculator version 2</h1>
-            <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
-          </header>
-          <div>
+//          <header className="App-header">
+//            <img src={logo} className="App-logo" alt="logo" />
+//            <h1 className="App-title">Fib Calculator version 2</h1>
+//            <Link to="/">Home</Link>
+//            <Link to="/otherpage">Other Page</Link>
+//          </header>
+            <div>
             <Route exact path="/" component={Fib} />
             <Route path="/otherpage" component={OtherPage} />
           </div>
